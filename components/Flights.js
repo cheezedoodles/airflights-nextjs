ed4c85b191f69c0ae2e6954bba71744abfcd38e9
cheezedoodles <hougvapes@gmail.com>
2023-02-16 22:15:54 +0300

    updated span styles in Flights.js

--- components/Flights.js
+++ components/Flights.js
@@ -16,19 +16,28 @@ export default function Flights() {
 
   return (
     <ul>
+      <li className={styles.item}>
+        <span style={{width: '20%'}}>flight_no</span>
+        <span style={{width: '25%'}}>departure_airport</span>
+        <span style={{width: '20%'}}>arrival_airport</span>
+        <span style={{width: '20%'}}>status</span>
+        <span style={{width: '10%'}}>aircraft_code</span>
+        <span style={{width: '35%'}}>actual_departure</span>
+        <span style={{width: '35%'}}>actual_arrival</span>
+      </li>
       {data.map((item) => (
         <li className={styles.item} key={item.flight_id}>
-          <span style={{width: '10%'}}>{item.flight_no}</span>
-          <span style={{width: '10%'}}>{item.departure_airport}</span>
-          <span style={{width: '10%'}}>{item.arrival_airport}</span>
-          <span style={{width: '10%'}}>{item.status}</span>
+          <span style={{width: '20%'}}>{item.flight_no}</span>
+          <span style={{width: '25%'}}>{item.departure_airport}</span>
+          <span style={{width: '20%'}}>{item.arrival_airport}</span>
+          <span style={{width: '20%'}}>{item.status}</span>
           <span style={{width: '10%'}}>{item.aircraft_code}</span>
-          <span style={{width: '20%'}}>
+          <span style={{width: '35%'}}>
             {item.actual_departure 
               ? <Date dateString={item.actual_departure} /> 
               : 'TBA'}
           </span>
-          <span style={{width: '20%'}}>
+          <span style={{width: '35%'}}>
           {item.actual_arrival 
               ? <Date dateString={item.actual_arrival} /> 
               : 'TBA'}
