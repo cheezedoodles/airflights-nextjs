904bec228423f656a51929abf8c8a26ae0d6093b
cheezedoodles <hougvapes@gmail.com>
2023-02-19 16:27:34 +0300

    removed Header styles

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,9 @@ import styles from '@/styles/Header.module.css'
 
 export default function Header() {
   return (
-    <div className={styles.header}>
+    <div>
       <h1 className={styles.h1}>airflights</h1>
-      <p>blazingly fast</p>
+      <p className={styles.h1}>blazingly fast</p>
     </div>
   )
 } 
