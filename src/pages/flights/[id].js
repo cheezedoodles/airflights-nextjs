0c1802c957b0809b9fcad71e5934cbdf6b76d0ec
cheezedoodles <hougvapes@gmail.com>
2023-02-20 21:23:39 +0300

    created getStaticPaths

--- src/pages/flights/[id].js
+++ src/pages/flights/[id].js
@@ -1,3 +1,13 @@
+import { getFlightsIds } from '../../lib/getFlightsIds'
+
+export async function getStaticPaths() {
+  const paths = await getFlightsIds()
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
 export default function FlightInfo() {
   return <>hello world</>
