19d61cfbe0908047e7f50b8040334ae7f7f1dd94
cheezedoodles <hougvapes@gmail.com>
2023-02-21 14:36:22 +0300

    created a table for flightInfo

--- src/pages/flights/[id].js
+++ src/pages/flights/[id].js
@@ -1,12 +1,43 @@
 import { getFlightsIds, getFlightInfo } from '../../../lib/flights'
+import Date from 'components/Date'
 
 export default function FlightInfo({ flightInfo }) {
   return (
     <div>
-      {flightInfo.flightInfo.flight_id}
-      {flightInfo.flightInfo.flight_no}
-      {flightInfo.flightInfo.departure_airport_name}
-      {flightInfo.flightInfo.arrival_airport_name}
+      <table>
+        <tr>
+          <th>Flight No.</th>
+          <th>Departure airport</th>
+          <th>Departure airport name</th>
+          <th>Departure city</th>
+          <th>Arrival airport</th>
+          <th>Arrival airport name</th>
+          <th>Arrival city</th>
+          <th>Status</th>
+          <th>Scheduled departure</th>
+          <th>Scheduled arrival</th>
+        </tr>
+        <tr>
+          <td>{flightInfo.flightInfo.flight_no}</td>
+          <td>{flightInfo.flightInfo.departure_airport}</td>
+          <td>{flightInfo.flightInfo.departure_airport_name}</td>
+          <td>{flightInfo.flightInfo.departure_city}</td>
+          <td>{flightInfo.flightInfo.arrival_airport}</td>
+          <td>{flightInfo.flightInfo.arrival_airport_name}</td>
+          <td>{flightInfo.flightInfo.arrival_city}</td>
+          <td>{flightInfo.flightInfo.status}</td>
+          <td>
+            <Date 
+              dateString={flightInfo.flightInfo.scheduled_departure}
+            />
+          </td>
+          <td>
+            <Date 
+              dateString={flightInfo.flightInfo.scheduled_arrival}
+            />
+          </td>
+        </tr>
+      </table>
     </div>
   )
 }
