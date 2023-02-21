4e05ddf8131c351171262f78adcb51438937ecfc
cheezedoodles <hougvapes@gmail.com>
2023-02-21 14:56:03 +0300

    changed html structure in FlightInfo

--- src/pages/flights/[id].js
+++ src/pages/flights/[id].js
@@ -1,44 +1,58 @@
-import { getFlightsIds, getFlightInfo } from '../../../lib/flights'
 import Date from 'components/Date'
+
+import { getFlightsIds, getFlightInfo } from '../../../lib/flights'
+
+import styles from '@/styles/FlightInfo.module.css'
 
 export default function FlightInfo({ flightInfo }) {
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Flight No.</th>
-          <th>Departure airport</th>
-          <th>Departure airport name</th>
-          <th>Departure city</th>
-          <th>Arrival airport</th>
-          <th>Arrival airport name</th>
-          <th>Arrival city</th>
-          <th>Status</th>
-          <th>Scheduled departure</th>
-          <th>Scheduled arrival</th>
-        </tr>
-        <tr>
-          <td>{flightInfo.flightInfo.flight_no}</td>
-          <td>{flightInfo.flightInfo.departure_airport}</td>
-          <td>{flightInfo.flightInfo.departure_airport_name}</td>
-          <td>{flightInfo.flightInfo.departure_city}</td>
-          <td>{flightInfo.flightInfo.arrival_airport}</td>
-          <td>{flightInfo.flightInfo.arrival_airport_name}</td>
-          <td>{flightInfo.flightInfo.arrival_city}</td>
-          <td>{flightInfo.flightInfo.status}</td>
-          <td>
-            <Date 
-              dateString={flightInfo.flightInfo.scheduled_departure}
-            />
-          </td>
-          <td>
-            <Date 
-              dateString={flightInfo.flightInfo.scheduled_arrival}
-            />
-          </td>
-        </tr>
-      </table>
-    </div>
+    <>
+      <div>
+        <table>
+          <thead>
+           <tr>
+              <td>Flight No.</td>
+              <td>Departure airport</td>
+              <td>Departure airport name</td>
+              <td>Departure city</td>
+              <td>Arrival airport</td>
+              <td>Arrival airport name</td>
+              <td>Arrival city</td>
+              <td>Status</td>
+              <td>Scheduled departure</td>
+              <td>Scheduled arrival</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{flightInfo.flightInfo.flight_no}</td>
+              <td>{flightInfo.flightInfo.departure_airport}</td>
+              <td>{flightInfo.flightInfo.departure_airport_name}</td>
+              <td>{flightInfo.flightInfo.departure_city}</td>
+              <td>{flightInfo.flightInfo.arrival_airport}</td>
+              <td>{flightInfo.flightInfo.arrival_airport_name}</td>
+              <td>{flightInfo.flightInfo.arrival_city}</td>
+              <td>{flightInfo.flightInfo.status}</td>
+              <td>
+                <Date 
+                  dateString={flightInfo.flightInfo.scheduled_departure}
+                />
+              </td>
+              <td>
+                <Date 
+                  dateString={flightInfo.flightInfo.scheduled_arrival}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.seats}>
+        {flightInfo.allSeats.map((seat) => 
+          <button key={seat.seat_no}>{seat.seat_no}</button>
+        )}
+      </div>
+    </>
   )
 }
 
